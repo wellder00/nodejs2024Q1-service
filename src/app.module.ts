ce32be8e47e7d9fec4d22d1abce0584e57f1ff56
wellder00 <wellder00@gmail.com>
2024-03-06 19:58:09 +0200

    feat: add REST API points

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { DbModule } from './db/db.module';
+import { AlbumModule } from './album/album.module';
+import { ArtistModule } from './artist/artist.module';
+import { FavoriteModule } from './favorite/favorite.module';
+import { TrackModule } from './track/track.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, DbModule, AlbumModule, ArtistModule, FavoriteModule, TrackModule],
   controllers: [AppController],
   providers: [AppService],
 })
